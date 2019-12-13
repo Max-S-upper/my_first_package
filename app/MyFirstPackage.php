3057dfe4c41b5a440b1291b53853b7e9a79aa6aa
Max <maxim.sokolsky@gmail>
2019-12-13 09:45:13 +0200

    Add MyFirstPackage function Returns message with all processors or that you haven't got any processors yet

--- app/MyFirstPackage.php
+++ app/MyFirstPackage.php
@@ -1,9 +1,11 @@
 <?php
 
-
-class MyFirstPackage
+class MyFirstPackage extends \Monolog\Logger
 {
-    public function smile() {
-        echo ':)';
+    public function customGetProcessor() {
+        $processors = '';
+        implode($processors, $this->getProcessors());
+        if (!$processors) return "You haven't got any processors yet";
+        else return 'Here are your processor(s): ' . $processors;
     }
 }
